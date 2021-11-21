45b0a530106b7f8bf55dc5b73a60367e7875cb94
systemilc <email>
2021-11-21 14:34:31 -0300

    acrescentada fase 2 - personagem 1 e corrigido js com a resposta correta para a fase 1

--- js/personagem_3_fase1.js
+++ js/personagem_3_fase1.js
@@ -1,7 +1,7 @@
 function pergunta(){
    var resposta = prompt("Para onde quer viajar?")
 
-   if(resposta == 1 ){
+   if(resposta == 3 ){
     window.location.href="./personagem_3_fase2.html"
 }else{
  window.location.href="./game_over.html"
