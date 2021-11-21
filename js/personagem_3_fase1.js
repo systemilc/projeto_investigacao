de352fa50d7abbc5e3a481d4d386903114b89620
systemilc <email>
2021-11-21 14:30:38 -0300

    adicionada primeira fase personagem 3 e arquivos js do persoagem 3

---
+++ js/personagem_3_fase1.js
@@ -0,0 +1,15 @@
+function pergunta(){
+   var resposta = prompt("Para onde quer viajar?")
+
+   if(resposta == 1 ){
+    window.location.href="./personagem_3_fase2.html"
+}else{
+ window.location.href="./game_over.html"
+}
+}
+
+
+ 
+
+
+
