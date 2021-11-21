b0b3c2cd26bef0721e0cbe2095eb4351194d7f45
systemilc <email>
2021-11-21 15:21:15 -0300

    corrigido redirecionamento fase 3 para 4 personagem 3

--- js/personagem_3_fase3.js
+++ js/personagem_3_fase3.js
@@ -2,7 +2,7 @@ function pergunta(){
    var resposta = prompt("Para onde quer viajar?")
 
    if(resposta == 1 ){
-    window.location.href="./personagem_2_fase4.html"
+    window.location.href="./personagem_3_fase4.html"
 }else{
  window.location.href="./game_over.html"
 }
