205a132ec869e0bd79b363e62b08e7d6c155ea62
systemilc <email>
2021-11-21 12:51:22 -0300

    criadas as fases de 2 a 4 para o personagem 2 - criados arquivos js para as respectivas fases

---
+++ js/personagem_2_fase2.js
@@ -0,0 +1,15 @@
+function pergunta(){
+   var resposta = prompt("Para onde quer viajar?")
+
+   if(resposta == 2 ){
+    window.location.href="./personagem_2_fase3.html"
+}else{
+ window.location.href="./game_over.html"
+}
+}
+
+
+ 
+
+
+
