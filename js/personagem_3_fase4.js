c682f5e81fa1b8bae00d0f0bb5236213ed479020
systemilc <email>
2021-11-21 14:51:44 -0300

    adicionada fase 3

--- js/personagem_3_fase4.js
+++ js/personagem_3_fase4.js
@@ -1,7 +1,7 @@
 function pergunta(){
    var resposta = prompt("Para onde quer viajar?")
 
-   if(resposta == 3 ){
+   if(resposta == 1 ){
     window.location.href="./vitoria.html"
 }else{
  window.location.href="./game_over.html"
