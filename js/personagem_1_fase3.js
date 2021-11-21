4a6bd2ecb4b3dc323c2690752f4763cc6b93d784
systemilc <email>
2021-11-21 15:15:10 -0300

    corrigido tag span na fase 4 personagem 1

--- js/personagem_1_fase3.js
+++ js/personagem_1_fase3.js
@@ -1,7 +1,7 @@
 function pergunta(){
    var resposta = prompt("Para onde quer viajar?")
 
-   if(resposta == 1 ){
+   if(resposta == 2 ){
     window.location.href="./personagem_1_fase4.html"
 }else{
  window.location.href="./game_over.html"
