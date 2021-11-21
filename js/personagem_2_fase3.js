3f1575e5a7d9c880a2c3952d74f5a8b6284e2413
systemilc <email>
2021-11-21 20:45:19 -0300

    adicionado while fases personagem 2

--- js/personagem_2_fase3.js
+++ js/personagem_2_fase3.js
@@ -1,13 +1,34 @@
-function pergunta(){
-   var resposta = prompt("Para onde quer viajar?")
+var resposta
 
-   if(resposta == 1 ){
-    window.location.href="./personagem_2_fase4.html"
-}else{
- window.location.href="./game_over.html"
-}
-}
+        function pertunta() {
 
+        while(resposta != 1 || resposta != 2 || resposta != 3 ) {
+
+        resposta = prompt("Digite o número correspondente ao país de destino")
+
+        if (resposta == 1){
+            window.location.href="./personagem_2_fase4.html"
+            break
+        }
+        else if (resposta == 3){
+
+            window.location.href='./game_over.html'
+            break
+        }
+		else if (resposta == 2){
+
+           window.location.href='./game_over.html'
+            break
+        }
+        else{
+            alert('Digite 1, 2 ou 3')
+           break
+
+        }
+    }
+
+
+    }
 
  
 
